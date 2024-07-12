148b8abf1a6aeadb27e52c6dbfe7f405c9dfff57
grover bhumika <grover.bhumika91@gmail.com>
2024-07-12 17:58:04 +0530

    html

---
+++ src/app/app.routes.ts
@@ -0,0 +1,34 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ServicesComponent } from './components/services/services.component';
+
+export const routes: Routes = [
+      {
+        path: '',
+        component: HomeComponent,
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      {
+        path: 'about',
+        component: AboutComponent,
+      },
+      {
+        path: 'blog',
+        component: BlogComponent,
+      },
+      {
+        path: 'contact',
+        component: ContactComponent,
+      },
+      {
+        path: 'services',
+        component: ServicesComponent,
+      },
+
+];
