da09489e8a7eb359a63275a9e1ea638fb94eebfe
grover bhumika <grover.bhumika91@gmail.com>
2024-08-11 13:33:09 +0530

    Ai gen

---
+++ src/app/components/gen-ai/gen-ai.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gen-ai',
+  standalone: true,
+  imports: [],
+  templateUrl: './gen-ai.component.html',
+  styleUrl: './gen-ai.component.scss'
+})
+export class GenAiComponent {
+
+}
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-header',
   standalone: true,
@@ -8,5 +8,16 @@ import { RouterModule } from '@angular/router';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+
+  constructor(public router:Router){
+    console.log(this.router.url)
+
+  }
+
+  ngOnInit(){
+    console.log(this.router.url)
+  }
+
+  
 
 }
