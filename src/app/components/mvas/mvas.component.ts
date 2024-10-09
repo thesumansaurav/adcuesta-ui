57fe67861c37274eff1493387a6678e3b08251c4
grover bhumika <grover.bhumika91@gmail.com>
2024-10-09 19:01:53 +0530

    service

---
+++ src/app/components/mvas/mvas.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mvas',
+  standalone: true,
+  imports: [],
+  templateUrl: './mvas.component.html',
+  styleUrl: './mvas.component.scss'
+})
+export class MvasComponent {
+
+}
